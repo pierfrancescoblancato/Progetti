1775eefa6df5d87882b43e623c6de92b270c0ecf
pierfrancescoblancato <pierfrancescoblancato436@gmail.com>
2026-01-25 10:41:25 +0100

    Modifica a square2

--- ProgettiStevejobs/C/primiEsercizi/square2.c
+++ ProgettiStevejobs/C/primiEsercizi/square2.c
@@ -2,16 +2,23 @@
 
 int main() {
     
-    int row,i=1;
-    printf("enter a Number: ");
+    int row,square,i=1;
+
+    printf("Enter a Number greater of 0: ");
     scanf("%d", &row);
     
-    while(row>0){
-      
-        int square = i*i;
+    if(row>0){
+        while(row>0){
+
+        square = i*i;
         printf("the square of %d is %d\n", i, square);
          
         row--;
         i++;
     }
+
+    }else{
+        printf("Row must be bigger than 0\n");
+    }
+    
 }
